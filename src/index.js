e9a21caa2f06cc5cb5654ccaa037ca4a20ab22c9
ncaudill27 <nelson.a.caudill@gmail.com>
2020-02-28 22:21:46 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+
+function getDogImages() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(json => renderDogImages(json))    
+}
+
+function renderDogImages(json) {
+    let imgSrcArray = json["message"]
+    imgSrcArray.map(imgSrc => addDogImage(imgSrc))
+}
+
+function addDogImage(src) {
+    let imageContainer = document.getElementById('dog-image-container')
+    let image = document.createElement('img')
+    image.src = src
+    imageContainer.
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    getDogImages()
+})
