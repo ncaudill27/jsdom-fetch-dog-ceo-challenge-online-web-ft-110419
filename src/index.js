3d1eefed4bda42e4b2e41195380d285bc29edd9f
ncaudill27 <nelson.a.caudill@gmail.com>
2020-02-28 22:49:20 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function getDogImages() {
 }
 
 function renderDogImages(json) {
-    let imgSrcArray = json["message"]
+    let imgSrcArray = json.message
     imgSrcArray.map(imgSrc => addDogImage(imgSrc))
 }
 
@@ -15,9 +15,30 @@ function addDogImage(src) {
     let imageContainer = document.getElementById('dog-image-container')
     let image = document.createElement('img')
     image.src = src
-    imageContainer.
+    imageContainer.appendChild(image)
+}
+
+function getDogBreeds() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(json => renderDogBreeds(json.message))
+}
+
+function renderDogBreeds(breeds) {
+    for (breed in breeds) {
+        addDogBreed(breed)
+    }
+    // breeds.map(breed => addDogBreed(breed))
+}
+
+function addDogBreed(breed) {
+    let breedContainer = document.getElementById('dog-breeds')
+    let li = document.createElement('li')
+    li.innerText = breed
+    breedContainer.appendChild(li)
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
     getDogImages()
+    getDogBreeds()
 })
