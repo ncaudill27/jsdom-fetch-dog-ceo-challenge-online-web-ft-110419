08da93ea47b37a2ec24b45b0f3562dd00a28a0ce
ncaudill27 <nelson.a.caudill@gmail.com>
2020-02-29 23:34:55 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ function getDogImages() {
 }
 
 function renderDogImages(json) {
-    let imgSrcArray = json.message
+    const imgSrcArray = json.message
     imgSrcArray.map(imgSrc => addDogImage(imgSrc))
 }
 
 function addDogImage(src) {
-    let imageContainer = document.getElementById('dog-image-container')
-    let image = document.createElement('img')
+    const imageContainer = document.getElementById('dog-image-container')
+    const image = document.createElement('img')
     image.src = src
     imageContainer.appendChild(image)
 }
@@ -26,25 +26,50 @@ function renderDogBreeds(breeds) {
     for (breed in breeds) {
         addDogBreed(breed)
     }
-    // breeds.map(breed => addDogBreed(breed))
 }
 
 function addDogBreed(breed) {
-    let breedContainer = document.getElementById('dog-breeds')
-    let li = document.createElement('li')
+    const breedContainer = document.getElementById('dog-breeds')
+    const li = document.createElement('li')
     li.innerText = breed
     breedContainer.appendChild(li)
 }
 
+function arrayOfBreeds() {
+    const dogList = document.querySelectorAll('ul > li')
+    const dogArray = Array.from(dogList)
+    const dogBreeds = dogArray.map(dog => {return dog.innerText})
+    return dogBreeds
+}
+
+function matchFirstLetter(word, letter) {
+    return word.charAt(0) === letter
+}
+
+function filter() {
+    
+    const dogs = arrayOfBreeds()
+    const letter = document.getElementById('breed-dropdown').value
+    const filtered = dogs.filter(function(dog) {
+        return dog.charAt(0) === letter
+    })
+    
+    const breedContainer = document.getElementById('dog-breeds')
+    while( breedContainer.firstChild ){
+        breedContainer.removeChild( breedContainer.firstChild );
+    }
+    filtered.map(breed => addDogBreed(breed))
+}
+
 function addEventListeners() {
-    let breedContainer = document.getElementById('dog-breeds')
+    const breedContainer = document.getElementById('dog-breeds')
     breedContainer.addEventListener('click', function(evt) {
         if (evt.target && evt.target.matches('li')) {
             evt.target.style.color = 'lightblue'
         }
     })
 }
-console.log('%c HI', 'color: firebrick')
+
 document.addEventListener('DOMContentLoaded', ()=>{
     getDogImages()
     getDogBreeds()
